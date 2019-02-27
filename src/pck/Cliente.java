8c43c76ab74c4e5c66ba65c7098407fc6aecc880
Andrea Carolina Lopez <ac.lopez@uniandes.edu.co>
2019-02-27 16:01:39 -0500

    Documento

--- src/pck/Cliente.java
+++ src/pck/Cliente.java
@@ -6,15 +6,11 @@ public class Cliente extends Thread{
 	private short id;
 	private Stack<Mensaje> mensajes = new Stack<Mensaje>();
 	private Buffer buffer;
-	
-	
-	
 	public Cliente(short nId, Buffer nBuffer) {
 		super();
 		this.id = nId;
 		this.buffer= nBuffer;
 	}
-
 	/*
 	 * genera un numero aleatorio de mensajes y los mete a la lista en el buffer
 	 */
@@ -29,13 +25,10 @@ public class Cliente extends Thread{
 				e.printStackTrace();
 			}
 			System.out.println("El Cliente con id "+id+" envia el mensaje: "+m.getMensaje());
-		}
-		
-			
+		}	
 	}
 	
 	private void generarMensajes (){
-
 		short numeroMensajes =  (short) ((short) (Math.random()*5)+1);
 		numeroMensajes ++;
 		for (int i = 0; i < numeroMensajes; i++) {
